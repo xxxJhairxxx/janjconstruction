fe8a3f77027039c4d0f127c87fd899b296ce1f96
cesarzv <cesarzegarrav26@gmail.com>
2024-02-12 15:11:36 -0500

    header

--- app/components/ui/Header.tsx
+++ app/components/ui/Header.tsx
@@ -1,5 +1,5 @@
 import { useNavbarContext } from '@/context/navbar.context'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { Container } from '../globals/Container'
 import { Logo } from '../atoms/Logo'
 import { MenuIcon } from '../atoms/MenuIcon'
@@ -8,8 +8,6 @@ import { Navbar } from '../molecules/Navbar'
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false)
   const { isTopZero } = useNavbarContext()
-
-  const [width, setWith] = useState<number>(0)
 
   const toggleMenu = () => {
     setIsMenuOpen((prevState) => !prevState)
@@ -21,17 +19,10 @@ const Header = () => {
     document.body.classList.remove('no-scroll')
   }
 
-  useEffect(() => {
-    setWith(window.innerWidth)
-  }, [])
-
   return (
     <header className={`Header ${isTopZero ? 'top' : ''}`}>
       <Container className='Header-ctn'>
-        <Logo
-          className={`Header-Logo
-        ${isTopZero ? 'header-fixed' : ''}`}
-        />
+        <Logo className={`Header-Logo`} />
         <Logo className='Header-Logo2' />
 
         <div className={`Header-menuIcon ${isMenuOpen && 'isActive'}`}>
